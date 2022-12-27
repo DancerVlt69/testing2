768a306b2fe2aa37e933f631c302b4718c056679
DancerVlt69 <dancervlt69@googlemail.com>
2022-12-28 00:50:17 +0100

    cosmetics

--- src/main/java/com/coertzy/krophlayt/structures/KrophStructures.java
+++ src/main/java/com/coertzy/krophlayt/structures/KrophStructures.java
@@ -1,7 +1,6 @@
 package com.coertzy.krophlayt.structures;
 
 import com.coertzy.krophlayt.KrophMain;
-import com.coertzy.krophlayt.structures.KrophSkyStructures;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,6 +10,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class KrophStructures {
     public static final DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE =
             DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, KrophMain.MOD_ID);
+
     public static final RegistryObject<StructureFeature<?>> SKY_STRUCTURES =
             DEFERRED_REGISTRY_STRUCTURE.register("sky_structures", KrophSkyStructures::new);
 
